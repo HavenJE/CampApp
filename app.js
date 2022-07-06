6733b495b393ce83111dd767574537092f5a3636
haven <haven.eldebaque@gmail.com>
2022-07-06 23:59:09 +1000

    initial style pushed to Campground_Index_Route

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground'); 
+const Campground = require('./models/campground');
 
 // The name of the database is yelp-camp - then Colt passed our options e.g. useNewUrl but that for some reason caused Nodemon to crash! 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
-const db = mongoose.connection; 
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log('Database connected'); 
+    console.log('Database connected');
 });
 
 const app = express();
 
 
 //Setting up, the veiw engine - where path is the global object and __dirname holds current directory address. Views is the folder where our all web pages will be kept. 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: "My Backyard", description: "cheap camping"})
-    await camp.save(); 
-    res.send(camp)
+// Up to PART 412 
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({title: "My Backyard", description: "cheap camping"})
+//     await camp.save(); 
+//     res.send(camp)
+// })
+
+// Will set different routes for Campgrounds e.g. campgrounds index "/campgrounds"
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    // Then we pass that to a template 
+    res.render('campgrounds/index', { campgrounds })
 })
 
 app.listen(3000, () => {
