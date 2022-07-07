1f5e177c4fc89c9360281f5f44ede5cb006ab9bc
haven <haven.eldebaque@gmail.com>
2022-07-08 00:12:47 +1000

    initial style pushed to Campground_New_

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ db.once("open", () => {
 
 const app = express();
 
-
 //Setting up, the veiw engine - where path is the global object and __dirname holds current directory address. Views is the folder where our all web pages will be kept. 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+// The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) or the qs library (when true).
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -36,17 +38,28 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     // Then we pass that to a template called index.ejs within the campgrounds folder
     res.render('campgrounds/index', { campgrounds })
+});
+
+// New campground route (new)
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+// To set the endpoint, the /campgrounds as POST where the form is submitted 
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 // Campgrounds show route => eventually going to be the Details Page 
 app.get('/campgrounds/:id', async (req, res) => {
     // Then we pass that to a template 
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', { campground }); 
-})
+    res.render('campgrounds/show', { campground });
+});
 
 app.listen(3000, () => {
     console.log("Serving port 3000")
-})
+});
 
 
