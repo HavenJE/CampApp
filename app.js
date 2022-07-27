14b894c403fbbdc49d201e70e06437126c2a748a
haven <haven.eldebaque@gmail.com>
2022-07-27 19:45:55 +1000

    initial style pushed to Install_Express_Session

--- app.js
+++ app.js
@@ -41,6 +41,11 @@ const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false, 
     saveUninitialized: true, 
+    cookie: {
+        httpOnly: true, 
+        expire: Date.now() + 1000 * 60 * 60 * 24 * 7, 
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }
 app.use(session(sessionConfig))
 
